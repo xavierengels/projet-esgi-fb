8983bf045d4490cac82073392ce0ffb44fd75dbc
xavier engels <xavier.engels@doc-direct.com>
2015-04-08 16:27:06 +0200

    no message

--- index.php
+++ index.php
@@ -1,1 +1,67 @@
-test
+<?php
+ini_set('display_errors', 1);
+error_reporting(e_all);
+session_start();
+require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+FacebookSession::setDefaultApplication('449000611931438', '4081c73247e8a9729dc939b5fe6565c6');
+$helper = new FacebookRedirectLoginHelper('https://projet-esgi.herokuapp.com/');
+$loginUrl = $helper->getLoginUrl();
+
+
+if(isset($_SESSION) && isset($_SESSION['fb_token']))
+{
+	$session = new FacebookSession($_SESSION['fb_token']);
+	var_dump($session);
+}
+else
+{
+	   $session = $helper->getSessionFromRedirect();
+      
+}	
+
+?>
+<html>
+<head>
+</head>
+
+<body>
+
+
+  <?php
+  if($session)
+  {
+  	 $token = (string) $session->getAccessToken();
+     $_SESSION['fb_token'] = $token;
+  }
+  else
+  {
+  	 $loginUrl = $helper->getLoginUrl();
+     echo "<a href='".$loginUrl."'>Se connecter</a>";
+  }
+           
+
+    
+   ?>
+</body>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '830895360333908',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/fr_FR/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+</html>
