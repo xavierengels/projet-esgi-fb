a09924d191911871951754915622e3d9f3bbf76a
Toumy <toumy.deng@gmail.com>
2015-04-20 11:02:56 +0200

    upload image
    upload image

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 
 
 <?php
+
+use Facebook\FacebookRequest;
+use Facebook\GraphObject;
+use Facebook\FacebookRequestException;
+
 include_once('conf.php');
 ini_set('display_errors', 1);
 error_reporting(e_all);
@@ -40,8 +45,36 @@ else
   }
       
 
-    
+    if($session) {
+
+  try {
+
+    // Upload to a user's profile. The photo will be in the
+    // first album in the profile. You can also upload to
+    // a specific album by using /ALBUM_ID as the path     
+    $response = (new FacebookRequest(
+      $session, 'POST', '/me/photos', array(
+        'source' => new CURLFile('path/to/file.name', 'image/png'),
+        'message' => 'User provided message'
+      )
+    ))->execute()->getGraphObject();
+
+    // If you're not using PHP 5.5 or later, change the file reference to:
+    // 'source' => '@/path/to/file.name'
+
+    echo "Posted with id: " . $response->getProperty('id');
+
+  } catch(FacebookRequestException $e) {
+
+    echo "Exception occured, code: " . $e->getCode();
+    echo " with message: " . $e->getMessage();
+
+  }   
+
+}
    ?>
+
+   
 </body>
 
 <script>
