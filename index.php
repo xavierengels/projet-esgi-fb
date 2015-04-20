238e1ac78bba0a27340701e8c9d59547d53a2e40
Toumy <toumy.deng@gmail.com>
2015-04-20 12:06:23 +0200

    r

--- index.php
+++ index.php
@@ -112,23 +112,7 @@ echo 'test';
    }(document, 'script', 'facebook-jssdk'));
 </script>
 
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '699704450158910',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/fr_FR/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 
 </html>
 
