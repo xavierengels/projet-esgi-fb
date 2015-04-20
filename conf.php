deef6b90e30ed298cf8af6a41192d7ed7b3c46c0
Baptiste Flesch <baptiste.flesch@gmail.com>
2015-04-20 11:05:08 +0200

    User info

--- conf.php
+++ conf.php
@@ -4,5 +4,8 @@
 require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
 
 ?>
--- index.php
+++ index.php
@@ -41,7 +41,24 @@ else
       
 
     
-   ?>
+   
+if($session) {
+
+    try {
+
+        $user_profile = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
+
+        echo "Name: " . $user_profile->getName();
+
+    } catch(FacebookRequestException $e) {
+
+        echo "Exception occured, code: " . $e->getCode();
+        echo " with message: " . $e->getMessage();
+
+    }   
+
+}
+?>
 </body>
 
 <script>
