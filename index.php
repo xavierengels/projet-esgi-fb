5cbfa78f7be642d6036116705e8853bc0311e2ac
Toumy <toumy.deng@gmail.com>
2015-04-20 08:56:11 +0200

    Revert "no message"
    This reverts commit f8edc2db2faa08047345ab8ae1f0f472bfa4af64.

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 
 <?php
-require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
+include_once('conf.php');
 ini_set('display_errors', 1);
 error_reporting(e_all);
 session_start();
