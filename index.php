6fbc0dd89c77a24a6678e2b3620ead11efc99333
xavier engels <xavier.engels@doc-direct.com>
2015-04-21 09:59:43 +0200

    no message

--- index.php
+++ index.php
@@ -100,11 +100,11 @@ echo "<a href='".$loginUrl."'>Se connecter</a>";
 
 if($session) {
     try {
-        $user_profile = (new FacebookRequest(
-        $session, 'GET', '/me'
-        ))->execute()->getGraphObject(GraphUser::className());
-
-        echo "Name: " . $user_profile->getName();
+      $_SESSION['fb_token'] = (string) $session->getAccessToken();
+        $request_user = new FacebookRequest( $session,"GET","/me");
+        $request_user_executed = $request_user->execute();
+        $user = $request_user_executed->getGraphObject(GraphUser::className());
+        echo "Bonjour ".$user->getName();
     } catch(FacebookRequestException $e) {
         echo "Exception occured, code: " . $e->getCode();
         echo " with message: " . $e->getMessage();
