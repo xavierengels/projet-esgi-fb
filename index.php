d808c679678b5ea00cf3f749f56683f6b36a6a95
xavier engels <xavier.engels@doc-direct.com>
2015-04-21 10:22:32 +0200

    no message

--- index.php
+++ index.php
@@ -44,14 +44,7 @@ echo "<a href='".$loginUrl."'>Se connecter</a>";
 }(document, 'script', 'facebook-jssdk'));</script>
 
 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=449000611931438";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
 
 <body>
   <?php
