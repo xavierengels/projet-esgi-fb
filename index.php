14c13cb630aab9b343ff53f4e93b5291ecf66312
xavier engels <xavier.engels@doc-direct.com>
2015-04-21 11:51:51 +0200

    no message

--- index.php
+++ index.php
@@ -94,30 +94,7 @@ if($session) {
 
 
 
-function UploadPhoto($session, $files)
-{
-  try {                  
-    $response = (new FacebookRequest(
-      $session, 'POST', '/me/photos', array(
-        'source' => $files,
-        'message' => 'User provided message'
-      )
-    ))->execute()->getGraphObject();
-    echo "Posted with id: " . $response->getProperty('id');
-  } catch(FacebookRequestException $e) {
-    echo "Exception occured, code: " . $e->getCode();
-    echo " with message: " . $e->getMessage();
-  } 
-}
 
- if(isset($_FILES) && isset($_FILES['source']))
-  {
-
-    
-    print_r($_FILES['source']);
-    UploadPhoto($session,$_FILES['source']);
-
-  }
 ?>
 
 
@@ -125,6 +102,50 @@ function UploadPhoto($session, $files)
 </body>
 
 <script>
+
+ function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '449000611931438',
