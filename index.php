682c72113dc76060e9eee536466c0337cb2f5aa1
xavier engels <xavier.engels@doc-direct.com>
2015-06-02 11:02:07 +0200

    test

--- index.php
+++ index.php
@@ -29,10 +29,6 @@ else
 
 } 
 
-
-
-
-
 ?>
 <html>
 <head>
@@ -74,9 +70,34 @@ else
 
 
      <?php 
-   
 
+     if($session) {
 
+  try {
+
+    // Upload to a user's profile. The photo will be in the
+    // first album in the profile. You can also upload to
+    // a specific album by using /ALBUM_ID as the path     
+    $response = (new FacebookRequest(
+      $session, 'POST', '/me/photos', array(
+        'source' => new CURLFile('path/to/file.name', 'image/png'),
+        'message' => 'User provided message'
+      )
+    ))->execute()->getGraphObject();
+
+    // If you're not using PHP 5.5 or later, change the file reference to:
+    // 'source' => '@/path/to/file.name'
+
+    echo "Posted with id: " . $response->getProperty('id');
+
+  } catch(FacebookRequestException $e) {
+
+    echo "Exception occured, code: " . $e->getCode();
+    echo " with message: " . $e->getMessage();
+
+  }   
+
+}
 
 if($session) {
     try {
