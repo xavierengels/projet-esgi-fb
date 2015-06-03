f3b8f7a1ca0a6de8c89deae2696caaacef46b848
xavier engels <xavier.engels@doc-direct.com>
2015-06-03 16:29:48 +0200

    no message

--- index.php
+++ index.php
@@ -22,7 +22,8 @@ $user =  'blnwydiaqtvkyp';
 $pass =  'yODIF2ML7nUOjWl-jBPkS54hHw';
 try {
     $dbh = new PDO("pgsql:host=ec2-54-247-118-153.eu-west-1.compute.amazonaws.com;dbname=d7fa01u2c92h52", $user, $pass);
-    $q = $dbh->prepare("DESCRIBE liste");
+    $q = $dbh->prepare("select column_name, data_type, character_maximum_length
+                        from INFORMATION_SCHEMA.COLUMNS where table_name = 'liste'");
     $q->execute();
     $table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
     print_r($table_fields);
