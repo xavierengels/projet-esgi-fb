139a49b7d9f36a6d09e5836ff2f26099e41b542a
xavier engels <xavier.engels@doc-direct.com>
2015-06-03 14:24:13 +0200

    no message

--- lesmeilleurs.php
+++ lesmeilleurs.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <script src="https://projet-esgi-fb.herokuapp.com/jquery-2.1.4.min.js"></script>
     <link rel="stylesheet" href="https://projet-esgi-fb.herokuapp.com/bootstrap-3.3.4-dist/css/bootstrap.min.css">
 
 <!-- Optional theme -->
