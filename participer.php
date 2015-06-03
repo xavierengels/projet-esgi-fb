1917220083ba73e7fcb10e3832ee81781f552ab1
Rayvexx <baptiste.flesch@outlook.com>
2015-06-03 14:41:24 +0200

    CSS

--- participer.php
+++ participer.php
@@ -22,8 +22,8 @@
 
             
                       <ul class="nav nav-pills">
-  <li role="presentation" class="active"><a href="index.php">Acceuil</a></li>
-  <li role="presentation"><a href="participer.php">Participer</a></li>
+  <li role="presentation"><a href="index.php">Acceuil</a></li>
+  <li role="presentation" class="active"><a href="participer.php">Participer</a></li>
   <li role="presentation"><a href="plusrecents.php">Recents</a></li>
     <li role="presentation"><a href="lesmeilleurs.php">Populaire</a></li>
 
