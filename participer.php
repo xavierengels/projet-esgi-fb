467b255c4b6cca5b1be0a406d4e45ed9e7f5f31e
xavier engels <xavier.engels@doc-direct.com>
2015-07-14 17:06:10 +0200

    no message

--- participer.php
+++ participer.php
@@ -7,43 +7,15 @@ ini_set('display_errors', 1);
 error_reporting('e_all');
 session_start();
 $session = $_SESSION['fb_token'];
-
+include('pages/header.php');
+include('pages/menu.php');
 
 
 
 echo "test session : ".$session;
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <script src="https://projet-esgi-fb.herokuapp.com/jquery-2.1.4.min.js"></script>
-    <link rel="stylesheet" href="https://projet-esgi-fb.herokuapp.com/bootstrap-3.3.4-dist/css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://projet-esgi-fb.herokuapp.com/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://projet-esgi-fb.herokuapp.com/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Document sans nom</title>
-</head>
-
-<body>
-<nav id="nav">
-    <div class="navbar">
-        <div class="navbar-inner">
-            <div class="container">
 
 
-
-                <ul class="nav nav-pills">
-
-                    <li role="presentation"><a href="index.php">Acceuil</a></li>
-                    <li role="presentation" class="active"><a href="participer.php">Participer</a></li>
-                    <li role="presentation"><a href="plusrecents.php">Recents</a></li>
-                    <li role="presentation"><a href="lesmeilleurs.php">Populaire</a></li>
-
-                </ul>
                 <form method="post" action="" enctype="multipart/form-data">
                     <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
                     <input type="file" name="source" id="source" /><br />
@@ -104,10 +76,7 @@ echo "test session : ".$session;
                 }else
                 {
                     echo "Aucune session";
-                }?>
-            </div>
-        </div>
-    </div>
-</nav>
-</body>
-</html>
+                }
+                include('pages/footer.php');
+                ?>
+
