8fbe7741f398d5a190ef9d41f90ca3658bf2144a
xavier engels <xavier.engels@doc-direct.com>
2015-07-14 16:12:05 +0200

    test connect

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xavierengels
+ * Date: 14/07/15
+ * Time: 15:57
+ */
+define('APP_ID',"449000611931438");
+define('APP_SECRET',"4081c73247e8a9729dc939b5fe6565c6");
+define('FB_URL_SITE',"https://projet-esgi-fb.herokuapp.com/");
