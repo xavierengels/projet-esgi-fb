20c7c33a9a5daab22f34d3f04d90ad8aba89478f
xavier engels <xavier.engels@doc-direct.com>
2015-07-14 22:22:38 +0200

    no message

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ if($session) {
         $user = $request_user_executed->getGraphObject(GraphUser::className());
         $request = new FacebookRequest( $session,"GET","/me/photos");
         $response = $request->execute();
-        $album_data =  $response->getPropertyAsArray();
+        $album_data =  $response->getPropertyNames();
         print_r($album_data);
         /*$albums = $response->getGraphObject();
         $album_data =  $user_profile->getProperty('data');
