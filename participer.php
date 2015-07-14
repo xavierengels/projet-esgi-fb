40f90caa061e860d68e4760043aa824595a92df8
xavier engels <xavier.engels@doc-direct.com>
2015-07-14 19:54:12 +0200

    no message

--- participer.php
+++ participer.php
@@ -18,8 +18,20 @@ $request = new FacebookRequest(
     'GET',
     '/me/photos'
 );
-$response = $request->execute();
-$graphObject = $response->getGraphObject();
+
+try {
+    $response = $request->execute();
+    $graphObject = $response->getGraphObject();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
     print_r($graphObject);
 ?>
 
