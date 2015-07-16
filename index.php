4d596da5c61d1348c2ed214d4c80413eb8b36b33
xavier engels <xavier.engels@doc-direct.com>
2015-07-16 19:59:20 +0200

    no message

--- index.php
+++ index.php
@@ -100,13 +100,27 @@ if($session) {
 
         $albums = getAlbums($session, 'me');
         if($_POST['show_photos'] == '1'){
-            echo "POST !!!".$_POST['album_id'];
+            /*echo "POST !!!".$_POST['album_id'];
             $listPhotos = getPhotos($session, 'me', $_POST['album_id']);
             print_r($listPhotos);
             foreach($listPhotos as $photo){
                 echo "<img src='{$photo->getProperty("source")}' />", "<br />";
                 echo "??????????????";
-            }
+            }*/
+            //$albums = getAlbums($session, 'me');
+            print_r($albums);
+            for ($i = 0; null !== $albums->getProperty('data')->getProperty($i); $i++) {
+                $album = $albums->getProperty('data')->getProperty($i);
+                $request = new FacebookRequest($session, 'GET', '/'.$album->getProperty('id').'/photos');
+                $response = $request->execute();
+                $photos = $response->getGraphObject();
+
+                for ($j = 0; null !== $photos->getProperty('data')->getProperty($j); $j++) {
+                    if($album_id == null || $album_id == $album->getProperty('id')){
+                        $photo[] = $photos->getProperty('data')->getProperty($j);
+
+                    }
+                }
         }
 
 ?>
