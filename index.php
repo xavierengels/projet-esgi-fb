eff985aad2a3c9af26f04d5831ae053b3325730e
xavier engels <xavier.engels@doc-direct.com>
2015-07-16 22:34:07 +0200

    no message

--- index.php
+++ index.php
@@ -128,7 +128,7 @@ if($session) {
                 $request = new FacebookRequest($session, 'GET', '/' . $album->getProperty('id') . '/photos');
                 $response = $request->execute();
                 $photos = $response->getGraphObject();
-                print_r($photos->getProperty('data')->getPropertyAsArray() );
+                print_r($photos->getProperty('data') );
 
                 /*for ($j = 0; null !== $photos->getProperty('data')->getProperty($j); $j++) {
                     if ($album_id == null || $album_id == $album->getProperty('id')) {
