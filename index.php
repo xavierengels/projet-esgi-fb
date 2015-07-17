8916b66b3b706742c239414abfeac76ae72036c6
xavier engels <xavier.engels@doc-direct.com>
2015-07-17 16:41:24 +0200

    no message

--- index.php
+++ index.php
@@ -100,9 +100,9 @@ if($session) {
 
                     if($_POST['album_id']==$album->getProperty('id')) {
                         foreach ($photos as $picture) {
-
+                            echo ('<img src="' . $picture->getProperty('picture') . '" alt="" />');
                             echo('<option value='.$picture->getProperty('picture').'>'.$picture->getProperty('picture').'</option>');
-                           
+
                         }
                     }
                 }?>
