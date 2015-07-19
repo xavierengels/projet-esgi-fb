f8344a4aa44c44d707fd1d4b19188332cc9a706d
xavier engels <xavier.engels@doc-direct.com>
2015-07-20 01:23:54 +0200

    no message

--- index.php
+++ index.php
@@ -98,8 +98,12 @@ function uploadPhoto($session, $id_user){
     }
 }
 //si la session exite on recupère les info de l'utlisateur
-
-if($session && $_GET['page'] == '1') {
+$vote =$false;
+if($_POST['vote'] ==  '1')
+{
+    $vote =true;
+}
+if($session && $vote==false) {
     try {
 
         $_SESSION['fb_token'] = (string) $session->getAccessToken();
@@ -293,11 +297,7 @@ if($session && $_GET['page'] == '1') {
                 }
 
         }
-        if($_POST['vote']=='1' && $_GET['page'] == '2')
-        {
-            $vote = true;
-            ECHO "POST VOTE";
-        }
+
 
         ?>
         <form class="form-horizontal" enctype="multipart/form-data" method="POST" action="index.php">
@@ -342,14 +342,18 @@ if($session && $_GET['page'] == '1') {
     }
 
 }
+else if($session && $vote ==true)
+{
+
+}
 else
 {
     $loginUrl = $helper->getLoginUrl();
    // echo "<a href='".$loginUrl."'>Se connecter</a>";
     ?>
     <div>
-<a href="<?=$loginUrl?>?page=1" class="btn btn-block btn-lg btn-default">Je Participe</a>
-        <form class="form-horizontal" enctype="multipart/form-data" method="POST" action="<?=$loginUrl?>?page=2">
+<a href="<?=$loginUrl?>" class="btn btn-block btn-lg btn-default">Je Participe</a>
+        <form class="form-horizontal" enctype="multipart/form-data" method="POST" action="<?=$loginUrl?>">
             <button id="vote" name="vote" value="1" type="submit"class="btn btn-block btn-lg btn-default">Je Vote</button>
         </form>
 </div>
