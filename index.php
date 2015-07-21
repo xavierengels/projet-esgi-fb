5d92bc04ba9ed333ed0f60d1f3425b6a5e0df436
xavier engels <xavier.engels@doc-direct.com>
2015-07-21 18:54:28 +0200

    no message

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ function uploadPhoto($session, $id_user){
     }
 }
 //si la session exite on recupère les info de l'utlisateur
-if($session) {
+if($session && $_POST['vote'] != '1') {
     try {
         if(getPermission($session)){
             $request = new FacebookRequest($session, "GET", "/me");
