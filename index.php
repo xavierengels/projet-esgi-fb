ac7d7bceb66dd393bdb8b984c51975eee6e10deb
xavier engels <xavier.engels@doc-direct.com>
2015-07-22 01:11:49 +0200

    no message

--- index.php
+++ index.php
@@ -12,8 +12,37 @@ error_reporting('e_all');
 session_start();
 FacebookSession::setDefaultApplication(APP_ID, APP_SECRET);
 $helper = new FacebookRedirectLoginHelper(FB_URL_SITE);
+$helperr = $fb->getJavaScriptHelper();
 
+try {
+    $accessToken = $helperr->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
 
+if (! isset($accessToken)) {
+    echo 'No cookie set or no OAuth data could be obtained from cookie.';
+    exit;
+}
+
+// Logged in
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
+$_SESSION['fb_access_token'] = (string) $accessToken;
+
+// User is logged in!
+// You can redirect them to a members-only page.
+//header('Location: https://example.com/members.php');
+?>
+<p><a href="#" onClick="logInWithFacebook()">Log In with the JavaScript SDK</a></p>
+<?
 
 function getPermission($session)
 {
--- pages/header.php
+++ pages/header.php
@@ -10,20 +10,34 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://projet-esgi-fb.herokuapp.com/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+
     <script>
+        logInWithFacebook = function() {
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    alert('You are logged in & cookie set!');
+                    // Now you can redirect the user or do an AJAX request to
+                    // a PHP script that grabs the signed request from the cookie.
+                } else {
+                    alert('User cancelled login or did not fully authorize.');
+                }
+            });
+            return false;
+        };
         window.fbAsyncInit = function() {
             FB.init({
-                appId      : '449000611931438',
-                cookie      :true,
-                xfbml      : true,
-                version    : 'v2.3'
+                appId: '449000611931438',
+                cookie: true, // This is important, it's not enabled by default
+                version: 'v2.3'
             });
         };
+
         (function(d, s, id){
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) {return;}
             js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/fr_FR/sdk.js";
+            js.src = "//connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     </script>
