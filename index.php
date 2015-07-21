d5707ca2d40d118ecdbc6b7ecbe202799e239d8f
xavier engels <xavier.engels@doc-direct.com>
2015-07-21 20:03:38 +0200

    no message

--- index.php
+++ index.php
@@ -309,7 +309,7 @@ else if($_POST['vote']=='1' && $session)
         echo '<form class="form-horizontal" enctype="multipart/form-data" method="POST" action="index.php">';
         foreach ($liste as $key => $valListe)
         {
-            echo('Voter pour une photo : <input type="image" value="' .$valListe['user_photo']. '" name="icone" src="' .$valListe['user_photo']. '" alt="" ><button  value="1" type="submit" name="vote_photos">Vote</button>' . "</br>";
+            echo'Voter pour une photo : <input type="image" value="' .$valListe['user_photo']. '" name="icone" src="' .$valListe['user_photo']. '" alt="" ><button  value="1" type="submit" name="vote_photos">Vote</button>' . "</br>";
             echo '<input type="hidden" value="'.$valListe['nb_vote'].'" name="value_nb_vote" ></input>';
             echo '<div>Nombre de vote : '.$valListe['nb_vote'].'</div>';
 
@@ -331,7 +331,7 @@ else
 
 if($_POST['vote_photos'] == '1' && $session)
 {
-    echo $_POST['icone'];
+
     try{
         $dbh = new PDO("pgsql:host=ec2-54-247-118-153.eu-west-1.compute.amazonaws.com;port=5432;dbname=d7fa01u2c92h52", USER, PASS);
 
