eb2d14776974e8149c38d78392c5be7baaf9c8e9
xavier engels <xavier.engels@doc-direct.com>
2015-07-21 23:04:39 +0200

    no message

--- pages/header.php
+++ pages/header.php
@@ -10,12 +10,23 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://projet-esgi-fb.herokuapp.com/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <script>(function(d, s, id) {
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '449000611931438',
+                cookie      :true,
+                xfbml      : true,
+                version    : 'v2.3'
+            });
+        };
+        (function(d, s, id){
             var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
+            if (d.getElementById(id)) {return;}
             js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4&appId=449000611931438";
+            js.src = "//connect.facebook.net/fr_FR/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
 </head>
 <body>
