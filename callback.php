8a1cc0f1fa4e743053c603b24bc431967dd83745
xavier engels <xavier.engels@doc-direct.com>
2015-07-22 14:25:17 +0200

    no message

---
+++ callback.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+include('config.php');
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookCanvasLoginHelper;
+
+
+
+FacebookSession::setDefaultApplication(APP_ID, APP_SECRET);
+
+$helper = new FacebookRedirectLoginHelper('https://esgi-fb.herokuapp.com/callback.php');
+
+// Now you have the session
+$session = $helper->getSessionFromRedirect();
+$_SESSION['fb_token'] = $session->getToken();
+
+if($session) {
+    header("Location: https://esgi-fb.herokuapp.com/index.php");
+} else {
+    header("Location: https://esgi-fb.herokuapp.com");
+}
