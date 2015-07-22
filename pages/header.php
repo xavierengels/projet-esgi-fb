bd14244ed732a36dbe0ba928e74b41e075ae77e4
xavier engels <xavier.engels@doc-direct.com>
2015-07-22 10:31:36 +0200

    no message

--- pages/header.php
+++ pages/header.php
@@ -12,35 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
 
-    <script>
-        logInWithFacebook = function() {
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    alert('You are logged in & cookie set!');
-                    // Now you can redirect the user or do an AJAX request to
-                    // a PHP script that grabs the signed request from the cookie.
-                } else {
-                    alert('User cancelled login or did not fully authorize.');
-                }
-            });
-            return false;
-        };
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId: '449000611931438',
-                cookie: true, // This is important, it's not enabled by default
-                version: 'v2.3'
-            });
-        };
 
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
 
 </head>
 <body>
