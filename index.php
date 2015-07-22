125fc51488dbdb3009685f0cf1997ce078a4a9c6
xavier engels <xavier.engels@doc-direct.com>
2015-07-22 23:15:57 +0200

    no message

--- index.php
+++ index.php
@@ -390,7 +390,14 @@ if($_POST['regle'] == '1' && isset($session))
 
 
 
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4&appId=449000611931438";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 <?php
 
 include('pages/footer.php');
