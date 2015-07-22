8953a08fc36d0ffda23e729d881d29b430cd6d19
xavier engels <xavier.engels@doc-direct.com>
2015-07-22 15:01:35 +0200

    no message

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
  */
 require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
 define('APP_ID',"449000611931438");
-define('APP_SECRET',"4081c73247e8a9729dc939b5fe6565c6");
+define('APP_SECRET',"0e60c88d5065c07af43d99e1bb66aaca");
 define('FB_URL_SITE',"https://projet-esgi-fb.herokuapp.com/");
 define('USER',"blnwydiaqtvkyp");
 define('PASS','yODIF2ML7nUOjWl-jBPkS54hHw');
